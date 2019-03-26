6e18e100cf0fdff3e930e23a1e1b76a6c0dc69fe
ruchira peiris <ruchirapeiris7@gmail.com>
2019-03-27 05:22:51 +0530

    find number of mirror images which has more than three characters

--- Mirror.java
+++ Mirror.java
@@ -1,8 +1,39 @@
+import java.util.*;
+import java.io.*;
 class Mirror{
 	
 	public static void main(String[] args){
 		
-		System.out.println("Test");
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("insert word :");
+		String input=s.next();
+		System.out.println(input);
+		Stack stack=new Stack();
+		int count=0;
+		int subCount=0;
+		char previoustTop=0;
+		
+		for(int i=0;i<input.length();i++){
+			if(!stack.isEmpty())
+				previoustTop=(char)stack.peek();
+			stack.push(input.charAt(i));
+			if(previoustTop==(char)stack.peek()){
+				stack.pop();
+				stack.pop();
+				subCount++;
+			}
+			if(subCount>2){
+				count++;
+				subCount=0;
+			}
+			
+		}
+		
+		System.out.println(count);
+		
+	
+		
 		
 	}
 	
